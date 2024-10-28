86a4320af621d6ea724ea5a3e78a44e58b7c1411
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-29 05:23:00 +0530

    hey

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -43,8 +43,8 @@ console.log(stringNumber);
 
 //*************************************OPERATIONS *******************
 
-let value=3
-let negValue=-value
+let value = 3;
+let negValue = -value;
 console.log(negValue);
 
 // console.log(2+2);
@@ -54,23 +54,25 @@ console.log(negValue);
 // console.log(2/3);
 // console.log(2%3);
 
-let str1="hello"
-let str2=" hitesh"
-let str3=str1+str2
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1 + str2;
 console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2+2); //=>122 
-console.log(1+2+"2"); //32 
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //=>122
+console.log(1 + 2 + "2"); //32
 
 console.log(-true); //-1
-console.log(+"");//0
+console.log(+""); //0
 
-let num1,num2,num3,num4
+let num1, num2, num3;
 
+num1 = num2 = num3 = 2 + 2;
 
+console.table([num1, num2, num3]);
 
-
-
-
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
