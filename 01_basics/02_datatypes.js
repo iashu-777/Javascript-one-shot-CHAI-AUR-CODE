32524535e11111fc0ae3d6b0c8572d3ce8925da8
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-28 21:00:34 +0530

    hey

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") //we are using nodejs not browser
+
+console.log(3+3);
+console.log("hitesh");
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+
+//number => 2^53
+//bigint
+//string , can be used in single quote or double
+//boolean=>true/false
+//null=standalone value, it is a object
+//undefined=> value not assigned
+//symbol =>unique
+
+//object
+
+console.log(typeof undefined ); //unefined
+console.log(typeof null); //object
+
+
+
+
+
+
+
