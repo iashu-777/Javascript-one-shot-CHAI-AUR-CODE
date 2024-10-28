9502e63870b8004c07b5ac5d016a8e41a2cac16a
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-28 21:34:40 +0530

    hey

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,24 @@
-let score="33abc"
+let score = "33abc";
 console.log(typeof score);
 console.log(typeof score);
 
-let valueInNumber=Number(score)
+let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber); //it give NaN which means not a number. take care of this thing
 
-let score1=null
+let score1 = null;
 console.log(typeof score1);
 console.log(typeof score1);
 
-let valueInNumber1=Number(score1)
+let valueInNumber1 = Number(score1);
 console.log(typeof valueInNumber1);
 console.log(valueInNumber1); //it give 0
 
-let score2=undefined
+let score2 = undefined;
 console.log(typeof score2);
 console.log(typeof score2);
 
-let valueInNumber2=Number(score2)
+let valueInNumber2 = Number(score2);
 console.log(typeof valueInNumber2);
 console.log(valueInNumber2); //it give NaN which means not a number. take care of this thing
 
@@ -26,11 +26,51 @@ console.log(valueInNumber2); //it give NaN which means not a number. take care o
 //"33abc"=>NaN
 //true=>1;false=>0
 
-let isLoggedIn=""
+let isLoggedIn = "";
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
 //1=>true;0=>false
 //""=>false
 //"hitesh" =>true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+//*************************************OPERATIONS *******************
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); //=>122 
+console.log(1+2+"2"); //32 
+
+console.log(-true); //-1
+console.log(+"");//0
+
+let num1,num2,num3
+
+
+
+
+
+
