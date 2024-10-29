dddbf066a00b86dc5a23ea8d6fedf0f04688de80
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-29 07:35:37 +0530

    hey

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ const repoCOunt=50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCOunt}`);
 
-const gameName=new String('hitesh-hc')
+const gameName=new String('hitesh-hc-com')
 
 
 console.log(gameName[0]);
@@ -14,3 +14,20 @@ console.log(gameName.toUpperCase());
 
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
+
+const newString=gameName.substring(0,4)
+console.log(newString)
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne="     hitesh       "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('%'))
+
+console.log(gameName.split('-')); //array a jayega , we had split on the basis of dash
