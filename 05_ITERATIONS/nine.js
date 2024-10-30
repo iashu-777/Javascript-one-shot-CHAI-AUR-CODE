c0bd96a1a358ba7bc3505688a1202817d8cbb9ce
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-30 11:31:01 +0530

    hey

--- 05_ITERATIONS/nine.js
+++ 05_ITERATIONS/nine.js
@@ -9,3 +9,27 @@ const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentV
 //accumulator=accumulator+currentValue
 }
 console.log(sum); // Output: 15
+
+
+const shoppringCart=[
+    {
+        itemName:"js course",
+        price:2999
+    },
+    {
+        itemName:"python course",
+        price:999
+    },
+    {
+        itemName:"mobile dev course",
+        price:5999
+    },
+    {
+        itemName:"java course",
+        price:3999
+    }
+]
+
+const totalPrice=shoppringCart.reduce((accumulator,currentValue)=>accumulator+currentValue.price,0)
+
+console.log(totalPrice)
