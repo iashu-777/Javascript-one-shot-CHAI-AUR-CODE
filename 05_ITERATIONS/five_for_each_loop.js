6ff1222f83ba2dc4f210677499f817995a60e49d
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-30 10:39:41 +0530

    hey

--- 05_ITERATIONS/five_for_each_loop.js
+++ 05_ITERATIONS/five_for_each_loop.js
@@ -1,5 +1,44 @@
-const coding=["js","ruby","java","python","cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"];
 
-coding.forEach(element => {
-    console.log(element)
+coding.forEach((element) => {
+  console.log(element);
+});
+
+//another way------------------------------------------
+
+coding.forEach(function (val) {
+  console.log(val);
+});
+
+//another way------------------------------------------
+
+function printMe(item) {
+  console.log(item);
+}
+coding.forEach(printMe);
+
+//-------------------------------------------------------------
+
+coding.forEach((item, index, arr) => {
+  console.log(`index: ${index}, item: ${item}, array: ${arr}`);
+});
+
+const myCoding = [
+  { name: "js", level: "advanced" },
+  { name: "ruby", level: "beginner" },
+  { name: "java", level: "intermediate" },
+  { name: "python", level: "advanced" },
+  { name: "cpp", level: "beginner" },
+];
+
+myCoding.forEach((item) => {
+  console.log(`name: ${item.name}, level: ${item.level}`); //with dot operator we have fetched all values
+});
+
+myCoding.forEach((item) => {
+  console.log(item); //without dot operator we have fetched only object
+});
+
+myCoding.forEach((item, index) => {
+  console.log(`index: ${index}, item: ${item.name}`); // with index
 });
