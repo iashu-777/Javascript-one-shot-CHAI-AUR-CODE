7cd791fb634a4d91b9ada892b5f3420066ddbebd
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-30 08:39:01 +0530

    hey

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+//if
+const isUserLoggedIn=true
+if (isUserLoggedIn) {
+    console.log("user is logged in")
+}
+if(2==='2'){
+    console.log("executed")
+}
+// <,>,<=,>=,==,!=,=== triple equal also check type, same as this !==
+const temp=4
+if (temp<40)
+    console.log("yes")
+
+
+
+const score=200
+if(score>100)
+    var power="fly" //var globally define kar deta hai ,isliye isko if ke bahar access kar pa rahe hai
+    console.log(`User power ${power}`);
+console.log(`User power ${power}`)    
+
+
+const balance=1000
+
+if(balance>500) console.log("test") ,console.log("test2") //immature practise
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,9 @@
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+//asaan hai yrr same as c and cpp
