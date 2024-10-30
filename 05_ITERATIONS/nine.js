0f1d1deb99d7765eede06431a5d7b108cfbf8430
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-30 11:20:42 +0530

    hey

---
+++ 05_ITERATIONS/nine.js
@@ -0,0 +1,11 @@
+//reduce method
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+//reduce function algo:-
+//accumulator=0
+{
+//accumulator=accumulator+currentValue
+}
+console.log(sum); // Output: 15
