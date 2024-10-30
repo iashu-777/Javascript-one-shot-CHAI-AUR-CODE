b61b28787c9a8e813f8b3626bc405c7c24ce8528
iashu-777 <2k23.csds2310610@gmail.com>
2024-10-30 09:06:39 +0530

    hey

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,9 @@
-const userEmail="ashu@google.com" //if empty then maan liya jayega false , empty array ko true maana jayega
+const userEmail = "ashu@google.com"; //if empty then maan liya jayega false , empty array ko true maana jayega
 
-if(userEmail){
-    console.log("got user email")
-}
-else{
-    console.log("dont have user email")
+if (userEmail) {
+  console.log("got user email");
+} else {
+  console.log("dont have user email");
 }
 
 //falsy values
@@ -14,7 +13,18 @@ else{
 //truthy values
 //"0",'false'," ",[],{},function(){}
 
-const emptyObject={}
-if (Object.keys(emptyObject).length===0) { //for checking object is empty or not ,, Object.keys(emptyObject) give array
-    console.log("Object is empty")
+const emptyObject = {};
+if (Object.keys(emptyObject).length === 0) {
+  //for checking object is empty or not ,, Object.keys(emptyObject) give array
+  console.log("Object is empty");
 }
+
+//NUllish Coalescing Operator (??): null undefined ke liye use karte hai
+
+//ye operator agar null value aayi hai to uska safety check karta hai , database se null aaya undefined aaya to pura
+//code structure na bigde hamara isliye ye operator ka use karte hai
+let val1;
+// val1=5 ?? 10
+// val1 = null ?? 10;
+//val1=undefined??15
+console.log(val1);
